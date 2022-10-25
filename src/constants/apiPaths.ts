c069ffef837da533db222edad123b7acdf439a89
Ihar Bulaty <ihar.bulaty@lseg.com>
2022-10-25 21:56:15 +0300

    fix: [TASK-6] change import api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product:
-    "https://jpvt591yah.execute-api.eu-central-1.amazonaws.com/dev/v1/product-api",
+    "https://grt0ujei8g.execute-api.eu-central-1.amazonaws.com/dev/v1/product-import",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import:
-    "https://nakx85fecc.execute-api.eu-central-1.amazonaws.com/dev/v1/product-import",
+    "https://grt0ujei8g.execute-api.eu-central-1.amazonaws.com/dev/v1/product-import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
